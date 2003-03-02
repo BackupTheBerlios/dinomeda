50512ed7ac3b0b87de24741a60bf414377a39202
krake <krake>
2003-03-02 20:15:50 +0000

    Added help text.

--- dinomeda/src/demo/Dinomeda.java
+++ dinomeda/src/demo/Dinomeda.java
@@ -1,6 +1,6 @@
 ///////////////////////////////////////////////////////////////////////////////
 //
-// $Id: Dinomeda.java,v 1.1 2003/02/27 21:56:14 krake Exp $
+// $Id: Dinomeda.java,v 1.2 2003/03/02 20:15:50 krake Exp $
 //
 // Copyright: Kevin Krammer <voyager@sbox.tugraz.at>, 2002-2003
 //
@@ -76,31 +76,56 @@ public class Dinomeda
     int pos = -1;
     for (int count = 0; count < args.length; ++count)
     {
-      if (args[count].equals("--swing"))
+      if (args[count].equals("--help"))
+      {
+        printHelp();
+        System.exit(0);
+      }
+      else if (args[count].equals("--swing"))
       {
         use_swing_ = true;
-        
+
         pos = count;
         break;
       }
     }
-    
+
     if (pos > -1)
     {
       int length = args.length - 1;
 
       program_arguments_ = new String[length];
-            
+
       System.arraycopy(args, 0, program_arguments_, 0, pos);
-      
-      System.arraycopy(args, pos+1, program_arguments_, pos, (length-pos)); 
+
+      System.arraycopy(args, pos+1, program_arguments_, pos, (length-pos));
     }
     else
     {
       program_arguments_ = args;
     }
   }
-  
-  protected boolean use_swing_ = false;  
+
+  //---------------------------------------------------------------
+  /**
+   * Method description
+   */
+  private void printHelp()
+  {
+    System.out.println("Dinomeda Demo-Application");
+    System.out.println("usage console mode: Dinomeda [options] [files]");
+    System.out.println("usage swing mode: Dinomeda --swing [options]");
+    System.out.println();
+
+    System.out.println("General options:");
+    System.out.println("--cfg file\tuse the config file \"file\"");
+    System.out.println();
+
+    System.out.println("Console mode options:");
+    System.out.print("--metagrep string\tsearch all metadata of each file for the ");
+    System.out.println("given search string");
+  }
+
+  protected boolean use_swing_ = false;
   protected String[] program_arguments_;
 }
