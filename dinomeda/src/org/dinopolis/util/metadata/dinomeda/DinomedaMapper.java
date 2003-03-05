19b09f4f5c698ff8168be5e512fa3759acd0444b
osma <osma>
2003-03-05 12:56:52 +0000

    Documentation take two

--- dinomeda/src/org/dinopolis/util/metadata/dinomeda/DinomedaMapper.java
+++ dinomeda/src/org/dinopolis/util/metadata/dinomeda/DinomedaMapper.java
@@ -1,6 +1,6 @@
 ///////////////////////////////////////////////////////////////////////////////
 //
-// $Id: DinomedaMapper.java,v 1.2 2003/03/03 13:56:39 krake Exp $
+// $Id: DinomedaMapper.java,v 1.3 2003/03/05 12:56:52 osma Exp $
 //
 // Copyright: Martin Oswald <ossi1@sbox.tugraz.at>, 2002-2003
 // Copyright: Mattias Welponer <maba@sbox.tugraz.at>, 2002-2003
@@ -120,8 +120,7 @@ abstract public class DinomedaMapper implements DMDMapper
    *
    * @param item a value from the constants defined in this class
    *
-   * @return returns the item array if the specified field or null, if the
-   *         parameter was not one of the defined constants.
+   * @return returns the item array if the specified field exists or null,    * if the parameter was not one of the defined constants.
    */
   public Object[] get(int item)
   {
@@ -169,44 +168,161 @@ abstract public class DinomedaMapper implements DMDMapper
     return 0;
   }
   
-  abstract public String[] getTitle();
-  abstract public int getMaxTitleCount();
-  
+  //---------------------------------------------------------------
+  /**
+   * Gets item array of the Dinomeda title field.
+   * @return returns the title array 
+   */  abstract public String[] getTitle();
+  //---------------------------------------------------------------
+  /**
+   * Gets the maximum valid number of occurences of a title field.
+   * @return the maximum number of entries the title field can hold in
+   *         the format the mapper is mapping to.
+   */  abstract public int getMaxTitleCount();
+    //---------------------------------------------------------------
+  /**
+   * Gets item array of the Dinomeda creator field.
+   * @return returns the creator array 
+   */
   abstract public String[] getCreator();
-  abstract public int getMaxCreatorCount();
-  
+  //---------------------------------------------------------------
+  /**
+   * Gets the maximum valid number of occurences of a creator field.
+   * @return the maximum number of entries the creator field can hold in
+   *         the format the mapper is mapping to.
+   */  abstract public int getMaxCreatorCount();
+    //---------------------------------------------------------------
+  /**
+   * Gets item array of the Dinomeda date field.
+   * @return returns the date array 
+   */
   abstract public Date[] getDate();
-  abstract public int getMaxDateCount();
-  
+  //---------------------------------------------------------------
+  /**
+   * Gets the maximum valid number of occurences of a date field.
+   * @return the maximum number of entries the date field can hold in
+   *         the format the mapper is mapping to.
+   */  abstract public int getMaxDateCount();
+    //---------------------------------------------------------------
+  /**
+   * Gets item array of the Dinomeda description field.
+   * @return returns the description array 
+   */
   abstract public String[] getDescription();
-  abstract public int getMaxDescriptionCount();
-  
+  //---------------------------------------------------------------
+  /**
+   * Gets the maximum valid number of occurences of a description field.
+   * @return the maximum number of entries the tdescription field can hold in
+   *         the format the mapper is mapping to.
+   */  abstract public int getMaxDescriptionCount();
+    //---------------------------------------------------------------
+  /**
+   * Gets item array of the Dinomeda publisher field.
+   * @return returns the publisher array 
+   */
   abstract public String[] getPublisher();
-  abstract public int getMaxPublisherCount();
-  
+  //---------------------------------------------------------------
+  /**
+   * Gets the maximum valid number of occurences of a publisher field.
+   * @return the maximum number of entries the publisher field can hold in
+   *         the format the mapper is mapping to.
+   */  abstract public int getMaxPublisherCount();
+    //---------------------------------------------------------------
+  /**
+   * Gets item array of the Dinomeda contributor field.
+   * @return returns the contributor array 
+   */
   abstract public String[] getContributor();
-  abstract public int getMaxContributorCount();
-  
+  //---------------------------------------------------------------
+  /**
+   * Gets the maximum valid number of occurences of a contributor field.
+   * @return the maximum number of entries the contributor field can hold in
+   *         the format the mapper is mapping to.
+   */  abstract public int getMaxContributorCount();
+    //---------------------------------------------------------------
+  /**
+   * Gets item array of the Dinomeda type field.
+   * @return returns the type array 
+   */
   abstract public String[] getType();
-  abstract public int getMaxTypeCount();
-  
+  //---------------------------------------------------------------
+  /**
+   * Gets the maximum valid number of occurences of a type field.
+   * @return the maximum number of entries the type field can hold in
+   *         the format the mapper is mapping to.
+   */  abstract public int getMaxTypeCount();
+    //---------------------------------------------------------------
+  /**
+   * Gets item array of the Dinomeda language field.
+   * @return returns the language array 
+   */
   abstract public Locale[] getLanguage();
-  abstract public int getMaxLanguageCount();
-  
+  //---------------------------------------------------------------
+  /**
+   * Gets the maximum valid number of occurences of a language field.
+   * @return the maximum number of entries the language field can hold in
+   *         the format the mapper is mapping to.
+   */  abstract public int getMaxLanguageCount();
+    //---------------------------------------------------------------
+  /**
+   * Gets item array of the Dinomeda collection field.
+   * @return returns the collection array 
+   */
   abstract public String[] getCollection();
-  abstract public int getMaxCollectionCount();
-  
+  //---------------------------------------------------------------
+  /**
+   * Gets the maximum valid number of occurences of a collection field.
+   * @return the maximum number of entries the collection field can hold in
+   *         the format the mapper is mapping to.
+   */  abstract public int getMaxCollectionCount();
+    //---------------------------------------------------------------
+  /**
+   * Gets item array of the Dinomeda rights field.
+   * @return returns the rigths array 
+   */
   abstract public String[] getRights();
-  abstract public int getMaxRightsCount();
-  
+  //---------------------------------------------------------------
+  /**
+   * Gets the maximum valid number of occurences of a rights field.
+   * @return the maximum number of entries the rights field can hold in
+   *         the format the mapper is mapping to.
+   */  abstract public int getMaxRightsCount();
+    //---------------------------------------------------------------
+  /**
+   * Gets item array of the Dinomeda identifier field.
+   * @return returns the identifier array 
+   */
   abstract public URL[] getIdentifier();
-  abstract public int getMaxIdentifierCount();
-  
+  //---------------------------------------------------------------
+  /**
+   * Gets the maximum valid number of occurences of a identifier field.
+   * @return the maximum number of entries the identifier field can hold in
+   *         the format the mapper is mapping to.
+   */  abstract public int getMaxIdentifierCount();
+    //---------------------------------------------------------------
+  /**
+   * Gets item array of the Dinomeda source field.
+   * @return returns the source array 
+   */
   abstract public URL[] getSource();
-  abstract public int getMaxSourceCount();
-  
+  //---------------------------------------------------------------
+  /**
+   * Gets the maximum valid number of occurences of a source field.
+   * @return the maximum number of entries the source field can hold in
+   *         the format the mapper is mapping to.
+   */  abstract public int getMaxSourceCount();
+    //---------------------------------------------------------------
+  /**
+   * Gets item array of the Dinomeda relation field.
+   * @return returns the relation array 
+   */
   abstract public URL[] getRelation();
-  abstract public int getMaxRelationCount();
+  //---------------------------------------------------------------
+  /**
+   * Gets the maximum valid number of occurences of a relation field.
+   * @return the maximum number of entries the relation field can hold in
+   *         the format the mapper is mapping to.
+   */  abstract public int getMaxRelationCount();
   
   public void set(int item, Object[] value) throws ArrayIndexOutOfBoundsException
   {
@@ -225,30 +341,121 @@ abstract public class DinomedaMapper implements DMDMapper
     if (item==RELATION) setRelation((URL[])value);
   }
   
-  abstract public void setTitle(String[] title) throws ArrayIndexOutOfBoundsException;
-  
+  //---------------------------------------------------------------
+  /**
+   * Sets the Dinomeda title.
+   *
+   * @param title array of titles
+   *
+   * @throws ArrayIndexOutOfBoundsException if the length of the title array is   * larger than the maximum amount of titles the mapper is able to map.
+   */  abstract public void setTitle(String[] title) throws ArrayIndexOutOfBoundsException;
+    //---------------------------------------------------------------
+  /**
+   * Sets the Dinomeda creator.
+   *
+   * @param creator array of creators
+   *
+   * @throws ArrayIndexOutOfBoundsException if the length of the creator array is   * larger than the maximum amount of creators the mapper is able to map.
+   */
   abstract public void setCreator(String[] creator) throws ArrayIndexOutOfBoundsException;
-  
+    //---------------------------------------------------------------
+  /**
+   * Sets the Dinomeda date.
+   *
+   * @param date array of dates
+   *
+   * @throws ArrayIndexOutOfBoundsException if the length of the date array is   * larger than the maximum amount of dates the mapper is able to map.
+   */
   abstract public void setDate(Date[] date) throws ArrayIndexOutOfBoundsException;
-  
+    //---------------------------------------------------------------
+  /**
+   * Sets the Dinomeda title.
+   *
+   * @param description array of descriptions
+   *
+   * @throws ArrayIndexOutOfBoundsException if the length of the description array is   * larger than the maximum amount of descriptions the mapper is able to map.
+   */
   abstract public void setDescription(String[] description) throws ArrayIndexOutOfBoundsException;
-  
+    //---------------------------------------------------------------
+  /**
+   * Sets the Dinomeda publisher.
+   *
+   * @param publisher array of publishers
+   *
+   * @throws ArrayIndexOutOfBoundsException if the length of the publisher array is   * larger than the maximum amount of publishers the mapper is able to map.
+   */
   abstract public void setPublisher(String[] publisher) throws ArrayIndexOutOfBoundsException;
-  
+    //---------------------------------------------------------------
+  /**
+   * Sets the Dinomeda contributor.
+   *
+   * @param contributor array of contributors
+   *
+   * @throws ArrayIndexOutOfBoundsException if the length of the contributor array is   * larger than the maximum amount of contributors the mapper is able to map.
+   */
   abstract public void setContributor(String[] contributor) throws ArrayIndexOutOfBoundsException;
-  
+    //---------------------------------------------------------------
+  /**
+   * Sets the Dinomeda type.
+   *
+   * @param type array of types
+   *
+   * @throws ArrayIndexOutOfBoundsException if the length of the type array is   * larger than the maximum amount of types the mapper is able to map.
+   */
   abstract public void setType(String[] type) throws ArrayIndexOutOfBoundsException;
-  
+    //---------------------------------------------------------------
+  /**
+   * Sets the Dinomeda language.
+   *
+   * @param language array of language
+   *
+   * @throws ArrayIndexOutOfBoundsException if the length of the language array is   * larger than the maximum amount of languages the mapper is able to map.
+   */
   abstract public void setLanguage(Locale[] language) throws ArrayIndexOutOfBoundsException;
-  
+    //---------------------------------------------------------------
+  /**
+   * Sets the Dinomeda collection.
+   *
+   * @param collection array of collections
+   *
+   * @throws ArrayIndexOutOfBoundsException if the length of the collection array is   * larger than the maximum amount of collections the mapper is able to map.
+   */
   abstract public void setCollection(String[] collection) throws ArrayIndexOutOfBoundsException;
-  
+    //---------------------------------------------------------------
+  /**
+   * Sets the Dinomeda rights.
+   *
+   * @param rights array of rights
+   *
+   * @throws ArrayIndexOutOfBoundsException if the length of the rights array is   * larger than the maximum amount of rights the mapper is able to map.
+   */
   abstract public void setRights(String[] rights) throws ArrayIndexOutOfBoundsException;
-  
+    //---------------------------------------------------------------
+  /**
+   * Sets the Dinomeda identifier.
+   *
+   * @param identifier array of identifiers
+   *
+   * @throws ArrayIndexOutOfBoundsException if the length of the identifier array is   * larger than the maximum amount of identifiers the mapper is able to map.
+   */
   abstract public void setIdentifier(URL[] identifier) throws ArrayIndexOutOfBoundsException;
-  
+    //---------------------------------------------------------------
+  /**
+   * Sets the Dinomeda source.
+   *
+   * @param source array of sources
+   *
+   * @throws ArrayIndexOutOfBoundsException if the length of the source array is   * larger than the maximum amount of sources the mapper is able to map.
+   */
   abstract public void setSource(URL[] source) throws ArrayIndexOutOfBoundsException;
-  
+    //---------------------------------------------------------------
+  /**
+   * Sets the Dinomeda relation.
+   *
+   * @param relation array of relations
+   *
+   * @throws ArrayIndexOutOfBoundsException if the length of the relation array is   * larger than the maximum amount of relations the mapper is able to map.
+   */
   abstract public void setRelation(URL[] relation) throws ArrayIndexOutOfBoundsException;
   
   protected DMDStore store_;
